26079776992a937a6da7741a1e1529b10dfccd8c
MiroFerko <miroslav.ferko1@gmail.com>
2022-08-08 20:43:16 +0200

    Composition

--- src/Dog.java
+++ src/Dog.java
@@ -1,9 +1,20 @@
-public class Dog extends Pet {
+public class Dog implements Pet {
+
+    String name;
+    int age;
 
     public Dog(String name, int age) {
-        super(name, age);
+        this.name=name;
+        this.age=age;
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
     @Override
     public void describe() {
         System.out.println(this.name + " says bark! He is " + this.age + " years old.");
