cec471c06f2918021b25849d670f15dd1922a8a0
MiroFerko <miroslav.ferko1@gmail.com>
2022-08-08 20:44:17 +0200

    Composition

---
+++ src/Interface.java
@@ -0,0 +1,23 @@
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Benny", 2);
+        Cat myCat = new Cat("Gargfield", 3);
+        CrazyPet crazyPet = new CrazyPet();
+
+        myDog.describe();
+        myCat.describe();
+        crazyPet.describe();
+        
+        System.out.println();
+
+        Person per1 = new Person("Andrew");
+        Person per2 = new Person("Max");
+
+        per1.setPet(myDog);
+        per2.setPet(myCat);
+
+        System.out.println(per1.getName() + " has a pet named " + per1.getPet().getName());
+        System.out.println(per2.getName() + " has a pet named " + per2.getPet().getName());
+
+    }
+}
