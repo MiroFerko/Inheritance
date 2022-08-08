a5a552a32f8cb79699407c42fed53785b6dc6f43
MiroFerko <miroslav.ferko1@gmail.com>
2022-08-08 11:51:51 +0200

    Composition

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -6,5 +6,14 @@ public class Inheritance {
         myDog.describe();
         myCat.describe();
         System.out.println();
+
+        Person per1=new Person("Andrew");
+        Person per2=new Person("Max");
+
+        per1.setPet(myDog);
+        per2.setPet(myCat);
+
+        System.out.println(per1.getName()+ " has a pet named "+ per1.getPet().getName());
+        System.out.println(per2.getName()+ " has a pet named "+ per2.getPet().getName());
     }
 }
