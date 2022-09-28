b412dd53d19ee36a4a0292e4d4425fb4805f466c
MiroFerko <miroslav.ferko1@gmail.com>
2022-09-28 22:00:34 +0200

    Composition

--- src/Composition.java
+++ src/Composition.java
@@ -3,10 +3,6 @@ public class Composition {
         Dog myDog = new Dog("Benny", 2);
         Cat myCat = new Cat("Gargfield", 3);
 
-
-
-
-
         myDog.describe();
         myCat.describe();
         System.out.println();
